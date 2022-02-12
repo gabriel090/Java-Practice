7a9e28681229523db986cf9526ce62f272f1ce71
gabriel090 <gabriel.mbatha@gmail.com>
2022-02-12 09:44:43 +0300

    practice on java extends

---
+++ src/com/company/Calculations.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Calculations {
+    int c ;
+    public  void addition(int a, int b){
+        c = a + b;
+        System.out.println("Addition of two Numbers is "+c );
+    }
+    public  void subtraction(int a, int b){
+        c = a - b;
+        System.out.println("Subtraction of two Numbers is "+c );
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+
+public class Main {
+    //Extends is a keyword used to inherit the properties of a class. Below given the syntax of extends keyword
+    public static class My_Calculations extends  Calculations{
+        public void  multiplication(int a, int b){
+            c = a * b;
+            System.out.println("Multiplication of two Numbers is "+c );
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int a = 20; int b = 10;
+	My_Calculations demo = new My_Calculations();
+    demo.addition(a,b);
+    demo.subtraction(a,b);
+    demo.multiplication(a,b);
+    }
+}
